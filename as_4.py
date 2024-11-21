af7ff22a2d397fe43bf86ca8701651422224ff35
Devansh Bajpai <devanshb.code@gmail.com>
2024-11-21 21:39:51 +0530

    completed as_4

--- as_4.py
+++ as_4.py
@@ -51,3 +51,23 @@ for x in myStr:
 
 print(ans)
 
+
+# Solution 6
+fobj = open("sampleText.txt", 'r')
+
+myLines = fobj.readlines()
+linesCount = len(myLines)
+wordCount = 0
+charCount = 0
+for line in myLines:
+    words = line.split(" ")
+    wordCount += len(words)
+    
+    for word in words:
+        myWord = word.replace('\n', "")
+        charCount += len(myWord)
+
+
+print(linesCount, wordCount, charCount)
+
+fobj.close()
